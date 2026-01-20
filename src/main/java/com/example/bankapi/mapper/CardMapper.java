3187054908b97d448960aa37c64665fde65ebbe0
PAVEL FILONOV <pavel.filonov@weigandt-consulting.com>
2026-01-21 02:29:12 +0300

    Исправлен вывод номера карты, добавлена маска

--- src/main/java/com/example/bankapi/mapper/CardMapper.java
+++ src/main/java/com/example/bankapi/mapper/CardMapper.java
@@ -2,15 +2,26 @@ package com.example.bankapi.mapper;
 
 import com.example.bankapi.dto.CardReadDTO;
 import com.example.bankapi.entity.Card;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.ReportingPolicy.IGNORE;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = IGNORE)
-public interface CardMapper {
+public abstract class CardMapper {
+
+	private static final String MASK_REGEX = ".(?=.{4})";
 
 	@Mapping(source = "owner.id", target = "ownerId")
-	CardReadDTO toReadDto(Card entity);
+	@Mapping(target = "number", ignore = true)
+	public abstract CardReadDTO toReadDto(Card entity);
+
+	@AfterMapping
+	public void afterToReadDtoMapping(@MappingTarget CardReadDTO dto, Card entity) {
+		String maskedNumber = entity.getNumber().replaceAll(MASK_REGEX, "*");
+		dto.setNumber(maskedNumber);
+	}
 
 }
